5fd906a004dfcb49ec225f75af0d0cdcee45ed1b
nanitKh <nanit.khanna@artesian.io>
2025-07-07 12:01:22 +0530

    Add project files.

---
+++ StringConversion/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+String message = "";
+decimal total = 0m; 
+foreach (string value in values)
+{
+    decimal number;
+    if(decimal.TryParse(value,out number))
+    {
+        total += number;
+    }
+    else
+    {
+        message += value;
+    }
+}
+Console.WriteLine($"Total: {total}");
+Console.WriteLine(message);
