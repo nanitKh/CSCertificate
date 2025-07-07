9828a456bb2bd9744248a9f5a2dc5452d2d4a628
nanitKh <nanit.khanna@artesian.io>
2025-07-07 14:16:18 +0530

    Done String reversal, Substrings , made project of contoso application

--- StringReverse/Program.cs
+++ StringReverse/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+
+string []reversed = pangram.Split(' ');
+foreach(string word in reversed)
+{
+    char[] chars=word.ToCharArray();
+    Array.Reverse(chars);
+    Console.Write(new string(chars)+" ");
+}
---
+++ SubString/Program.cs
@@ -0,0 +1,4 @@
+﻿string message = "Hello , this is (Parenthesis)";
+int length = message.Length;
+string sub = message.Substring(17, length - 17);
+Console.WriteLine("Substring: " + sub);
