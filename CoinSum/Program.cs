947d40f176a0de05cf9e0cf742ed93af6e00c151
nanitKh <nanit.khanna@artesian.io>
2025-07-07 15:48:58 +0530

    Method calling , returning, parameter passing

---
+++ CoinSum/Program.cs
@@ -0,0 +1,27 @@
+﻿int target = 60;
+int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+int[] result = coinSum(coins, target);
+
+if (result.Length == 0)
+{
+    Console.WriteLine("No two coins found that sum to the target.");
+}
+else
+{
+    Console.WriteLine($"Coins that sum to {target}: {result[0]}, {result[1]}");
+}
+
+int[] coinSum(int[] coins,int target)
+{
+    for (int i = 0; i < coins.Length; i++)
+    {
+        for (int j = 0; j < coins.Length; j++)
+        {
+            if (coins[i] + coins[j] == target)
+            {
+                return new int[] { coins[i], coins[j] };
+            }
+        }
+    }
+    return new int[0];
+}
---
+++ MethodCalling/Program.cs
@@ -0,0 +1,67 @@
+﻿void SayHello(string name)
+{
+    Console.WriteLine($"Hello {name}!");
+}
+
+//SayHello("Nanit");
+
+
+
+//Generating random number with help of Methods
+
+void generatingRandomNumber()
+{
+    Random random = new Random();
+    int randomNumber = random.Next(1, 101);
+    Console.WriteLine($"Random number generated: {randomNumber}");
+}
+
+
+//generatingRandomNumber();
+
+/*
+if ipAddress consists of 4 numbers
+and
+if each ipAddress number has no leading zeroes
+and
+if each ipAddress number is in range 0 - 255
+
+then ipAddress is valid
+
+else ipAddress is invalid
+*/
+
+
+
+if (validateLength() && validateZeroes() && validateRange())
+{
+    Console.WriteLine("IP Address is valid");
+}
+else
+{
+    Console.WriteLine("IP Address is invalid");
+}
+
+bool validateLength(decimal ip)
+{
+    string[] ipParts = ip.ToString().Split('.');
+    return ipParts.Length == 4;
+}
+
+
+
+// Test method scope
+
+string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
+
+DisplayStudents(students);
+DisplayStudents(new string[] { "Robert", "Vanya" });
+
+void DisplayStudents(string[] students)
+{
+    foreach (string student in students)
+    {
+        Console.Write($"{student}, ");
+    }
+    Console.WriteLine();
+}
